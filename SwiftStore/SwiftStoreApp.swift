9d08d18275bf93454deb5853ec0f48c8d59e38d6
obaro Paul <105018533+bettercallpaul22@users.noreply.github.com>
2024-10-17 06:44:52 +0100

    Initial Commit

---
+++ SwiftStore/SwiftStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStoreApp.swift
+//  SwiftStore
+//
+//  Created by Obaro Paul on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
